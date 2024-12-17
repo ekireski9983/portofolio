3a8042c3b9a8d65da2a1506e7e17efba5e293634
ekireski9983 <rezkyjayadis@gmail.com>
2024-12-17 17:37:25 +0700

    update

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -12,7 +12,7 @@ let client
 let clientPromise
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Add Mongo URI to .env')
+  throw new Error('Add Mongo URI to .env.local')
 }
 
 if (process.env.NODE_ENV === 'development') {
