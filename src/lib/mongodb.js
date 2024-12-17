f5dbcf00a8b5cf1a73fee55d984847e26749c785
ekireski9983 <rezkyjayadis@gmail.com>
2024-12-17 17:35:36 +0700

    delete

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -12,7 +12,7 @@ let client
 let clientPromise
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Add Mongo URI to .env.local')
+  throw new Error('Add Mongo URI to .env')
 }
 
 if (process.env.NODE_ENV === 'development') {
